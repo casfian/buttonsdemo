346bd768b752e9b9b70d2ac8c422918b31d47332
BB Caspian <casfian@gmail.com>
2021-09-21 10:58:07 +0800

    add code to screen3

---
+++ lib/screen3.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Screen3 extends StatelessWidget {
+  const Screen3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen3'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //code kita
+          }, 
+          child: Text('Screen 3 goto Screen 2'),
+          ),
+      ),
+    );
+  }
+}
